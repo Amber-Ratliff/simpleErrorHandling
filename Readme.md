b6e11a191667fcd546c51347b02346fb9d200716
Amber Ratliff <aratliff1112@gmail.com>
2025-04-14 23:55:32 -0400

    added removal feature and added list creation to a seperate function

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 A project created to calculate the factorial of a number and display it, specifically to practice simple error handling.
 
 changed the alert system for results to be added to the html document.
+
+removes first list item if there are more than 5 results.
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //global variable definitions
 const button = document.getElementById('button');
 
-//function for input and simple error handling
+//function for input and error handling
 function errorCheck() {
     let num;
     do { 
@@ -18,6 +18,18 @@ function errorCheck() {
     while (true)
     }
 
+//append results to the DOM and display
+function createList(list, num, result) { 
+    const listItems = document.querySelectorAll('li');
+    let resultCount = listItems.length + 1;      
+    const listItem = document.createElement('li');
+    listItem.textContent = `The factorial for ${num} is: ${result}.`;
+    list.appendChild(listItem);
+    //removes the first list item if there are more than 5 results
+    if (resultCount > 5) {
+        list.removeChild(list.firstChild)};
+    return; 
+}
 function factorial(event) {
     const list = document.getElementById('factorial-results');
     let num = errorCheck();
@@ -25,12 +37,9 @@ function factorial(event) {
     for (let i = 1; i <= num; i++) {
         result *= i;
     }
-    const listItem = document.createElement('li');
-    listItem.textContent = `The factorial for ${num} is: ${result}.`;
-    list.appendChild(listItem);
+    createList(list, num, result);
     return result;
 }
-//initial function call
 button.addEventListener('click', factorial);
 
 
